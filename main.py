3d6bc9e72285b04add882748fb168d6ced794a1d
Tim Hirst <tim@hirst.com>
2021-05-22 13:25:17 +0100

    Get MP names and social media details

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
+from pprint import pprint
+
 import fiona
 from shapely.geometry import shape
+import requests
+import json
 
 fiona.drvsupport.supported_drivers['KML'] = 'rw'
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+
+def get_mp(constituency):
+    mp = requests.get(
+        f"https://www.theyworkforyou.com/api/getMP?"
+        f"constituency={constituency['name']}&"
+        f"output=js&"
+        f"key={config['TWFY_key']}")\
+        .json()
+    details = requests.get(
+        f"https://www.theyworkforyou.com/api/getMPInfo?"
+        f"id={mp['person_id']}&"
+        f"output=js&"
+        f"key={config['TWFY_key']}")\
+        .json()
+    return {
+               'full_name': mp['full_name'],
+               'twitter': f"https://twitter.com/{details['twitter_username']}" if details and 'twitter_username' in details else 'None',
+               'website': details['mp_website'] if details and 'mp_website' in details else 'None',
+               'facebook': details['facebook_page'] if details and 'facebook_page' in details else 'None'}
+
 
 if __name__ == '__main__':
     with fiona.open('Full_Run_to_Refuge_Route.kml') as source:
@@ -18,5 +46,13 @@ if __name__ == '__main__':
                 constituencies.append(constituency)
     with open('output.csv', 'w') as sink:
         for constituency in constituencies:
-            sink.write(f"{constituency['name']}\n")
+            mp = get_mp(constituency)
+            sink.write(
+                f"{constituency['id']},"
+                f"\"{constituency['name']}\","
+                f"\"{mp['full_name']}\","
+                f"{mp['twitter']},"
+                f"{mp['website']},"
+                f"{mp['facebook']}\n")
+
 
